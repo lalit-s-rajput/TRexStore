cdefaef2ce77afde396cee0ea98cc1e53bb27a71
Lalit <rajputlalit591@gmail.com>
2022-07-30 18:57:41 +0530

    feat: folder structure created

---
+++ TeeRexStore/src/app/result/containers/index.ts
@@ -0,0 +1,10 @@
+import {FiltersContainerComponent} from './filters-container/filters-container.component';
+import {ResultsContainerComponent} from './results-container/results-container.component';
+
+export const containers : any[] = [
+    ResultsContainerComponent,
+    FiltersContainerComponent
+];
+
+export * from './filters-container/filters-container.component';
+export * from './results-container/results-container.component';
---
+++ TeeRexStore/src/app/result/mock/mock-data.ts
@@ -0,0 +1,18 @@
+export const filterData = [
+    {
+        filterType: 'Color',
+        data: ['black','blue','pink']
+    },
+    {
+        filterType: 'Gender',
+        data: ['Men','Women']
+    },
+    {
+        filterType: 'Price',
+        data: ['0-200','200-400','450']
+    },
+    {
+        filterType: 'Type',
+        data: ['Polo','Hoodie','Basic']
+    },
+];
