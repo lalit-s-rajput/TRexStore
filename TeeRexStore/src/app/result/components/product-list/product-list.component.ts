f7467b9573f6b2d1ef6572247509b373e2d6c337
Lalit <rajputlalit591@gmail.com>
2022-07-31 15:40:17 +0530

    fix: toaster service added

--- TeeRexStore/src/app/result/components/product-list/product-list.component.ts
+++ TeeRexStore/src/app/result/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,11 +9,15 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
   _productData:any;
   filteredData:any;
+  resetFilter = false;
   @Input() set productData(value:any){
-    this._productData = value;
-    this.searchData('');
+    if(value){
+      this._productData = value;
+      this.filteredData = value;
+      this.searchData('');
+    }
   }
-  constructor() { }
+  constructor(private notification: NotificationService) { }
 
   ngOnInit(): void {
   }
@@ -24,8 +29,14 @@ export class ProductListComponent implements OnInit {
   }
 
   filterData(data:any){
-    this.filteredData = this._productData?.fillter((item:any)=>{
+    this.filteredData = this.filteredData?.filter((item:any)=>{
       return item[data.type.toLowerCase()].toLowerCase() === data.value.toLowerCase();
     });
+    this.resetFilter = false;
+    if(!this.filteredData.length){
+      this.filteredData = this._productData;
+      this.notification.showError('Result set is empty! Please try new filter or search.','Error!');
+      this.resetFilter = true;
+    }
   }
 }
--- TeeRexStore/src/app/result/services/result-service.service.ts
+++ TeeRexStore/src/app/result/services/result-service.service.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResultService {
+  initialState:any = {
+    color:[],
+    gender:[],
+    price:[],
+    tShirtType:[]
+  };
+  filterState = new BehaviorSubject(this.initialState);
   private dataUrl = 'https://geektrust.s3.ap-southeast-1.amazonaws.com/coding-problems/shopping-cart/catalogue.json';
   constructor(private http: HttpClient) { }
+
   getData(){
     return this.http.get(this.dataUrl);
+  }
+
+  getFilterState(){
+
+  }
+  setFilterState(type:string,value:string){
+    let modifiedFilteredData = {};
+    if(this.initialState[type.toLocaleLowerCase()]){
+      this.initialState.type.push(value);
+      modifiedFilteredData = {
+        ...this.initialState
+      };
+      return this.filterState.next(modifiedFilteredData);
+    }
   }
 }
