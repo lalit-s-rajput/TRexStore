376ba4899171a9bb1434e986ecd403ce0794ba59
lalit-s-rajput <67156745+lalit-s-rajput@users.noreply.github.com>
2026-03-22 01:16:54 +0530

    feat: migration to v18 completed

--- TeeRexStore/src/app/app.module.ts
+++ TeeRexStore/src/app/app.module.ts
@@ -7,28 +7,21 @@ import * as fromContainers from './result/containers';
 import * as fromComponents from './result/components';
 import { RouterModule } from '@angular/router';
 import {allRoutes} from './routes';
-import {HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { ReactiveFormsModule } from '@angular/forms';
-@NgModule({
-  declarations: [
-    AppComponent,
-    fromContainers.containers,
-    fromComponents.components,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    RouterModule.forRoot(allRoutes),
-    HttpClientModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        fromContainers.containers,
+        fromComponents.components,
+    ],
+    bootstrap: [AppComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterModule.forRoot(allRoutes),
+        BrowserAnimationsModule,
+        ToastrModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
